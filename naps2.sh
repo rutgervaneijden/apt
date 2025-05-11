295f949cd153d684efd8bdf9d3bcc85789236eaa
rutgervaneijden <63955794+rutgervaneijden@users.noreply.github.com>
2025-05-11 12:07:37 +0200

    Create naps2.sh

---
+++ naps2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -fsSL https://www.naps2.com/naps2-public.pgp | sudo gpg --dearmor -o /etc/apt/keyrings/naps2.gpg
+echo "deb [signed-by=/etc/apt/keyrings/naps2.gpg] https://downloads.naps2.com ./" | sudo tee /etc/apt/sources.list.d/naps2.list >/dev/null
+sudo apt update
+sudo apt install naps2
