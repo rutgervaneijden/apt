ffde781ddc2835310bd43f7f01230e70f8744237
rutgervaneijden <63955794+rutgervaneijden@users.noreply.github.com>
2025-05-11 12:31:32 +0200

    Update i3.sh

--- i3.sh
+++ i3.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y i3 xorg xrdp openssh-server
+sudo apt install -y i3 xorg xrdp openssh-server hplip wireshark firefox-esr thunderbird syncthing libreoffice-writer libreoffice-calc
+sudo usermod -aG lpadmin rutger
+sudo usermod -aG wireshark rutger
