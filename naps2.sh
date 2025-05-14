dd1fcb1f2b342cff81dc9fbe146b7e3f32546b6c
rutgervaneijden <63955794+rutgervaneijden@users.noreply.github.com>
2025-05-14 11:29:56 +0200

    Update naps2.sh

--- naps2.sh
+++ naps2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+sudo apt install -y curl
 curl -fsSL https://www.naps2.com/naps2-public.pgp | sudo gpg --dearmor -o /etc/apt/keyrings/naps2.gpg
 echo "deb [signed-by=/etc/apt/keyrings/naps2.gpg] https://downloads.naps2.com ./" | sudo tee /etc/apt/sources.list.d/naps2.list >/dev/null
 sudo apt update
-sudo apt install naps2
+sudo apt install -y naps2
